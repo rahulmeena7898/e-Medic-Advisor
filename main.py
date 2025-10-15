1f2a1a571cb5fb0f3c068103c032987b213516dd
rahulmeena7898 <rm497483@gmail.com>
2025-10-15 18:30:04 +0530

    Fix port for Render deployment

--- main.py
+++ main.py
@@ -117,4 +117,8 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)@app.route('/search', methods=['GET', 'POST'])
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
